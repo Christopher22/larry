f8ccbe240c2a1f739b02640b7e2bafe1a10c916f
Christopher Gundler <christopher@gundler.de>
2020-07-11 20:08:32 +0200

    Support 'force_reply'

--- larry/requests/SendMessage.php
+++ larry/requests/SendMessage.php
@@ -11,13 +11,23 @@ class SendMessage extends Request {
 	public function __construct(
 		Context $context,
 		User $user,
-		string $message
+		string $message,
+		bool $force_reply = false
 	) {
+		$request = array(
+			'chat_id' => $user->chat_id(),
+			'text'    => $message,
+		);
+
+		// Handle 'reply_markup'
+		if ( $force_reply === true ) {
+			$request['reply_markup'] = json_encode( array(
+				'force_reply' => true,
+			) );
+		}
+
 		parent::__construct( $context,
 			'sendMessage',
-			array(
-				'chat_id' => $user->chat_id(),
-				'text'    => $message,
-			) );
+			$request );
 	}
 }
