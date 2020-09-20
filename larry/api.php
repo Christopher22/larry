3e648afb94267fe601d9a66b0c31920f5406dfc2
Christopher Gundler <christopher@gundler.de>
2020-09-20 22:36:16 +0200

    Add API entry point

---
+++ larry/api.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace larry;
+
+use larry\api\Api;
+use larry\api\Request;
+use larry\api\Users;
+
+require "Context.php";
+
+// Create the context and fail if it is invalid
+$context = Context::create();
+if ( $context === null ) {
+	http_response_code( 500 );
+	error_log( "Larry: Context was NULL in index.php" );
+	exit();
+}
+
+// Create, parse, and respond the request
+$request  = Request::from_server( $context );
+$response = Api::parse( $request, array( new Users() ) );
+$response->send();
